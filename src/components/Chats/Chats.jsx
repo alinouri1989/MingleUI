71372f737e771e29d6fb37fe1795c67ad099112b
HamzaDogann <hamzadgn011@gmail.com>
2024-11-24 19:41:05 +0300

    Chat Component Started

--- src/components/Chats/Chats.jsx
+++ src/components/Chats/Chats.jsx
@@ -1,10 +1,50 @@
+import MessageInputBar from "../../shared/components/MessageInputBar/MessageInputBar";
 import WelcomeScreen from "../WelcomeScreen/WelcomeScreen";
+import { PiPhoneFill } from "react-icons/pi";
+import { HiMiniVideoCamera } from "react-icons/hi2";
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowDropleftCircle } from "react-icons/io";
+
 import "./style.scss";
 
+// Props ile kullanıcıyı alıcak. ya da url ile id üzerinden kullanıcı bilgisni alıcak hub durumu felan şimdilik statik
+
 function Chats() {
+
+  const userName = "Okan Doğan"
+  const status = "online"
+
   return (
     <div className='chat-general-box'>
-      <WelcomeScreen text={"Kişisel sohbetleriniz uçtan uca şifrelidir"}/>
+      {/* <WelcomeScreen text={"Kişisel sohbetleriniz uçtan uca şifrelidir"}/> */}
+
+      <div className="user-top-bar">
+        <div className="user-info">
+          <div className="image-box">
+            <img src="https://randomuser.me/api/portraits/men/1.jpg" alt={`${userName} profile`} />
+            <p className={`status ${status}`}></p>
+          </div>
+          <div className="name-and-status-box">
+            <p className="user-name">{userName}</p>
+            <span>{status == "online" ? "Çevrimiçi" : "Çevrimdışı"}</span>
+          </div>
+        </div>
+
+        <div className="top-bar-buttons">
+          <div className='call-options'>
+            <button><PiPhoneFill /></button>
+            <button><HiMiniVideoCamera /></button>
+          </div>
+          <IoIosArrowDropleftCircle className="show-sidebar-button" />
+        </div>
+      </div>
+      <div className="user-message-bar">
+
+      </div>
+      <MessageInputBar />
+      {/* <div className="user-details-sidebar">
+
+      </div> */}
     </div>
   )
 }
---
+++ src/shared/components/MessageInputBar/MessageInputBar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import "./style.scss";
+import { HiPlus } from "react-icons/hi";
+import { MdOutlineEmojiEmotions } from "react-icons/md";
+
+function MessageInputBar() {
+    return (
+        <div className='message-input-bar'>
+            <div className='input-box'>
+                <div className='add-file-box'>
+                    <button className='add-file-button'>
+                        <HiPlus />
+                    </button>
+                </div>
+                <input type="text" placeholder='Bir mesaj yazın' />
+                <div className='emoji-and-send-buttons'>
+                    <button className='add-emoji-button'>
+                        <MdOutlineEmojiEmotions />
+                    </button>
+                    <button className='send-message-button'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 27 27" fill="none">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M3.82724 7.50691C3.53474 4.88116 6.23812 2.95291 8.62649 4.08466L22.0635 10.4499C24.6375 11.6683 24.6375 15.3313 22.0635 16.5497L8.62649 22.916C6.23812 24.0478 3.53587 22.1195 3.82724 19.4938L4.36724 14.6248H13.5C13.7984 14.6248 14.0845 14.5063 14.2955 14.2953C14.5065 14.0843 14.625 13.7982 14.625 13.4998C14.625 13.2014 14.5065 12.9153 14.2955 12.7043C14.0845 12.4933 13.7984 12.3748 13.5 12.3748H4.36837L3.82724 7.50691Z" fill="white" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MessageInputBar
