dba66ba109de7ca214af91477dbf267febe93a9e
HamzaDogann <hamzadgn011@gmail.com>
2024-12-24 23:01:21 +0300

    Removed SignalR Provider on main.jsx
    Updated Approutes with SignalRProvider

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,11 @@ import { Provider } from 'react-redux';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ModalProvider } from './contexts/ModalContext.jsx';
-import { SignalRProvider } from './contexts/SignalRContext'; // SignalRProvider'ı buraya ekliyoruz
 import store from './store/index.js';
 import App from './App.jsx';
 import "./shared/styles/main.scss";
 
 createRoot(document.getElementById('root')).render(
-
-    <SignalRProvider>
         <BrowserRouter>
             <Provider store={store}>
                 <ModalProvider>
@@ -17,5 +14,4 @@ createRoot(document.getElementById('root')).render(
                 </ModalProvider>
             </Provider>
         </BrowserRouter>
-    </SignalRProvider>
 );
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -9,8 +9,9 @@ import Groups from '../components/Groups/Groups.jsx';
 import Calls from '../components/Calls/Calls.jsx';
 import Home from '../components/Home/Home.jsx';
 
-function AppRoutes() {
+import { SignalRProvider } from '../contexts/SignalRContext'; // SignalRProvider'ı burada kullanıyoruz.
 
+function AppRoutes() {
     const { user } = useSelector((state) => state.auth);
 
     return (
@@ -28,28 +29,24 @@ function AppRoutes() {
 
             {/* Protected Routes */}
             {user && (
-                <>
-                    {/* Giriş yapılmış kullanıcılar için yönlendirme */}
-                    <Route path="/" element={<Navigate to="/anasayfa" replace />} />
-
-                    {/* Layout ile korunan rotalar */}
-                    <Route path="/" element={<Layout />}>
-                        <Route path="anasayfa" element={<Home />} />
-                        <Route path="sohbetler" element={<Chats />} />
-                        <Route path="sohbetler/:id" element={<Chats />} />
-                        <Route path="arsivler" element={<Archives />} />
-                        <Route path="arsivler/:id" element={<Archives />} />
-                        <Route path="gruplar" element={<Groups />} />
-                        <Route path="gruplar/:id" element={<Groups />} />
-                        <Route path="aramalar" element={<Calls />} />
-                        <Route path="aramalar/:id" element={<Calls />} />
-                    </Route>
-
-                    {/* Giriş yapmış kullanıcı public rotalara erişmeye çalışırsa */}
-                    <Route path="/giris-yap" element={<Navigate to="/anasayfa" replace />} />
-                    <Route path="/uye-ol" element={<Navigate to="/anasayfa" replace />} />
-                    <Route path="/sifre-yenile" element={<Navigate to="/anasayfa" replace />} />
-                </>
+                <Route
+                    path="/"
+                    element={
+                        <SignalRProvider>
+                            <Layout />
+                        </SignalRProvider>
+                    }
+                >
+                    <Route path="anasayfa" element={<Home />} />
+                    <Route path="sohbetler" element={<Chats />} />
+                    <Route path="sohbetler/:id" element={<Chats />} />
+                    <Route path="arsivler" element={<Archives />} />
+                    <Route path="arsivler/:id" element={<Archives />} />
+                    <Route path="gruplar" element={<Groups />} />
+                    <Route path="gruplar/:id" element={<Groups />} />
+                    <Route path="aramalar" element={<Calls />} />
+                    <Route path="aramalar/:id" element={<Calls />} />
+                </Route>
             )}
 
             {/* Default: Tanımlı olmayan rotalar */}
