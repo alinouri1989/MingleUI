83c07c020f3536039ba1d8d0716e315ad3e1dc97
HamzaDogann <hamzadgn011@gmail.com>
2025-03-15 12:32:17 +0300

    Refactor startCall to improve validation error handling and adjust timeout duration

--- src/helpers/startCall.js
+++ src/helpers/startCall.js
@@ -4,26 +4,21 @@ import { ErrorAlert } from "./customAlert";
 export const startCall = async (callConnection, recipientId, isCameraCall, dispatch, openModalCallback) => {
     if (!callConnection || !recipientId) return;
 
-    let isCallBlocked = false; // Meşgul olup olmadığını takip eden flag
+    let isCallBlocked = false;
 
     try {
-        // ValidationError event'ini dinleyerek flag'i güncelliyoruz
         const handler = (data) => {
             ErrorAlert(data.message);
-            isCallBlocked = true; // Çağrı meşgul
+            isCallBlocked = true;
         };
         callConnection.on("ValidationError", handler);
 
-        // Çağrıyı başlat
         await callConnection.invoke("StartCall", recipientId, isCameraCall ? 1 : 0);
 
-        // Kısa bir süre bekleyerek ValidationError olup olmadığını kontrol ediyoruz
-        await new Promise((resolve) => setTimeout(resolve, 300));
+        await new Promise((resolve) => setTimeout(resolve, 200));
 
-        // Event listener'ı kaldır
         callConnection.off("ValidationError", handler);
 
-        // Eğer kullanıcı meşgulse, çağrıyı başlatmayacağız
         if (!isCallBlocked) {
             dispatch(setIsCallStarting(true));
             openModalCallback();
